9407332d20d523e8c00291282b4b7ed9c581ade0
Andon Mitev <andon@weichain.tech>
2025-10-18 22:59:20 +0300

    fix/build

--- lib/config/env.ts
+++ lib/config/env.ts
@@ -2,11 +2,7 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
-  server: {
-    OPENAI_API_KEY: z.string(),
-  },
+  server: {},
   client: {},
-  runtimeEnv: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
+  runtimeEnv: {},
 });
